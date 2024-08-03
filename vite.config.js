beced1da97d4b5ecfd2a3c15ca3ec2e6256618d8
Ayush Tandon <ayushtandon_co22a3_44@dtu.ac.in>
2024-08-03 13:02:00 +0530

    api issue fixed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import dotenv from 'dotenv';
 
-// https://vitejs.dev/config/
+// Load environment variables from src/.env
+dotenv.config({ path: resolve(__dirname, 'src/.env') });
+
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    'process.env': process.env
+  }
+});
